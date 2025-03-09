05a58c3b443243a121ff93b6e6a3f54166df4578
Bengt Brodersen <bengt.brodersen@gmail.com>
2025-03-09 20:45:14 +0100

    Update google-search-maps-link.user.js

--- google-search-maps-link.user.js
+++ google-search-maps-link.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Maps link for Google Search Results
 // @namespace    https://qoomon.github.io
-// @version      1.0.4
+// @version      1.0.5
 // @updateURL    https://github.com/qoomon/userscript-google-search-maps-link/raw/main/google-search-maps-link.user.js
 // @downloadURL  https://github.com/qoomon/userscript-google-search-maps-link/raw/main/google-search-maps-link.user.js
 // @author       qoomon
